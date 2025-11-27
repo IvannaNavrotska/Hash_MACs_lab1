f3659893d19f82bd190fcbaa81c088fcc1ea1318
Ivanna Navrotska <navrockaivanna15@gmail.com>
2025-11-27 22:18:48 +0200

    update pre

--- lab1.py
+++ lab1.py
@@ -8,7 +8,7 @@ def sha_384(message):
     m = message.encode()
     h = hashlib.sha384(m).hexdigest()
     
-    return 
+    return h
 
 
 def sha_384_16(message):
@@ -32,32 +32,32 @@ def sha_384_32(message):
 
 def pre():
 
-    my_message = 'Я Навроцька Іванна Аркадіївна'
+    my_message = 'Навроцька Іванна Аркадіївна'
     my_hash = sha_384(my_message)
-    hash_16bits = my_hash[-4:]
+    my_hash16 = my_hash[-4:]
 
-    n = 0
-    k = 0
+    n = 1
+    k = 1
 
-    with open(pre1_res.txt, 'w') as f:
+    with open('pre1_res.txt', 'w', encoding='utf-8') as f:
+
+        f.write(f'повідомлення: {my_message}, геш повідомлення: {my_hash}\n')
         
         new_message = my_message + str(n)
-        new_hash = sha_384(new_message)
-        new_hash_16bits = new_hash[-4:]
+        new_hash = sha_384_16(new_message)
 
-    
-        while new_hash_16bits != hash_16bits:
+        while new_hash != my_hash16:
 
-            if k < 30:
-                f.write(f'{n}: {new_message}, {new_hash}')
+            if k <= 30:
+                f.write(f'{n}: {new_message}, {new_hash}\n')
                 k += 1
                 
-        n += 1
-        new_message = my_message + str(n)
-        new_hash = sha_384(new_message)
-        new_hash_16bits = new_hash[-4:]
+            n += 1
+            new_message = my_message + str(n)
+            new_hash = sha_384_16(new_message)
         
-
+        f.write(f'останнє значення: {n}: {new_message}, {new_hash}')
+        
 
 pre()
 
