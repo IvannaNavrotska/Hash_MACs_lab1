d8b133d4679dced6f787009483bd8d25baec3248
Ivanna Navrotska <navrockaivanna15@gmail.com>
2025-11-27 23:32:45 +0200

    add pre2

--- lab1.py
+++ lab1.py
@@ -74,8 +74,61 @@ def pre1(messages):
     print('the end')
 
 
+#messages =  message_list()
+#pre1(messages)
+
+
+def random_changes(m):
+    
+    allowed = string.ascii_letters + string.digits + string.punctuation
+
+    m_list = list(m)
+    num_changes = random.randint(1, len(m_list))
+    positions = random.sample(range(len(m_list)), num_changes)
+
+    for pos in positions:
+        m_list[pos] = random.choice(allowed)
+
+    res = ''.join(m_list)
+
+    return res
+
+
+def pre2(messages):
+
+    with open('pre2_res.txt', 'w', encoding='utf-8') as f:
+
+        for i, my_message in enumerate(messages, start=1):
+
+            my_hash = sha_384(my_message)
+            my_hash16 = my_hash[-4:]
+
+            n = 1
+            k = 1
+      
+            f.write(f'повідомлення: {my_message}, геш повідомлення: {my_hash}\n')
+        
+            new_message = random_changes(my_message)
+            new_hash = sha_384_16(new_message)
+
+            while new_hash != my_hash16:
+
+                if k <= 30:
+                    f.write(f'{n}: {new_message}, {new_hash}\n')
+                    k += 1
+                
+                n += 1
+                new_message = random_changes(my_message)
+                new_hash = sha_384_16(new_message)
+        
+            f.write(f'останнє значення: {n}: {new_message}, {new_hash}')
+            f.write('\n\n')
+
+    print('the end')
+
+
 messages =  message_list()
-pre1(messages)
+pre2(messages)
 
 
 
