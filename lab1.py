31524274edb112aa9d6375a6ea8c062212496b8a
Ivanna Navrotska <navrockaivanna15@gmail.com>
2025-11-27 23:00:54 +0200

    update pre1

--- lab1.py
+++ lab1.py
@@ -30,36 +30,48 @@ def sha_384_32(message):
 
 
 
-def pre():
 
-    my_message = 'Навроцька Іванна Аркадіївна'
-    my_hash = sha_384(my_message)
-    my_hash16 = my_hash[-4:]
+def pre1():
 
-    n = 1
-    k = 1
+    pib = 'Навроцька Іванна Аркадіївна'
+    messages = []
+
+    for i in range(100):
+        piece = ''.join(random.choice(string.ascii_letters + string.digits) for j in range(5))
+        m = f'{pib} {piece}'
+        messages.append(m)
 
     with open('pre1_res.txt', 'w', encoding='utf-8') as f:
 
-        f.write(f'повідомлення: {my_message}, геш повідомлення: {my_hash}\n')
+        for i, my_message in enumerate(messages, start=1):
+
+            my_hash = sha_384(my_message)
+            my_hash16 = my_hash[-4:]
+
+            n = 1
+            k = 1
+      
+            f.write(f'повідомлення: {my_message}, геш повідомлення: {my_hash}\n')
         
-        new_message = my_message + str(n)
-        new_hash = sha_384_16(new_message)
-
-        while new_hash != my_hash16:
-
-            if k <= 30:
-                f.write(f'{n}: {new_message}, {new_hash}\n')
-                k += 1
-                
-            n += 1
             new_message = my_message + str(n)
             new_hash = sha_384_16(new_message)
-        
-        f.write(f'останнє значення: {n}: {new_message}, {new_hash}')
-        
 
-pre()
+            while new_hash != my_hash16:
+
+                if k <= 30:
+                    f.write(f'{n}: {new_message}, {new_hash}\n')
+                    k += 1
+                
+                n += 1
+                new_message = my_message + str(n)
+                new_hash = sha_384_16(new_message)
+        
+            f.write(f'останнє значення: {n}: {new_message}, {new_hash}')
+            f.write('\n\n')
+
+    print('the end')
+    
+pre1()
 
         
 
