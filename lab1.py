2aba389a3596fde9df38eda36925e21df3dcc1d7
Ivanna Navrotska <navrockaivanna15@gmail.com>
2025-11-27 19:57:30 +0200

    add sha-384

--- lab1.py
+++ lab1.py
@@ -1,1 +1,16 @@
-print('hello attack 1')
+import hashlib
+import random
+import string
+
+
+def sha_384(message):
+
+    m = message.encode()
+    h = hashlib.sha384(m).hexdigest()
+
+    return h
+
+
+print(sha_384('hello'))
+
+    
