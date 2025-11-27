013166d1ae7d4f034f5c8ac98c59122fee18e1f3
Ivanna Navrotska <navrockaivanna15@gmail.com>
2025-11-27 20:44:20 +0200

    add pre

--- lab1.py
+++ lab1.py
@@ -11,6 +11,40 @@ def sha_384(message):
     return h
 
 
-print(sha_384('hello'))
+my_message = 'Я Навроцька Іванна Аркадіївна'
+my_hash = sha_384(my_message)
+print(my_hash)
+
+
+def pre():
+
+    my_message = 'Я Навроцька Іванна Аркадіївна'
+    my_hash = sha_384(my_message)
+    hash_16bits = my_hash[-4:]
+
+    n = 1
+    new_message = my_message + str(n)
+    new_hash = sha_384(new_message)
+    new_hash_16bits = new_hash[-4:]
+
+    k = 1
+    
+    while new_hash_16bits != hash_16bits:
+        n += 1
+        new_message = my_message + str(n)
+        new_hash = sha_384(new_message)
+        new_hash_16bits = new_hash[-4:]
+        k += 1
+        print(k)
+        print(new_hash)
+
+
+pre()
+
+        
+
+    
+
+
 
     
